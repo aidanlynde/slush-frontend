70f118a9115f37dc63618bcb12932247cff38160
Aidan Lynde <130251073+aidanlynde@users.noreply.github.com>
2025-02-04 17:16:29 -0600

    Create shadows.ts
    Init themes

---
+++ src/theme/shadows.ts
@@ -0,0 +1,49 @@
+// src/theme/shadows.ts
+export const shadows = {
+    light: {
+      sm: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.18,
+        shadowRadius: 1.0,
+        elevation: 1,
+      },
+      md: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 3,
+      },
+      lg: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.30,
+        shadowRadius: 4.65,
+        elevation: 5,
+      },
+    },
+    dark: {
+      sm: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.3,
+        shadowRadius: 2.0,
+        elevation: 2,
+      },
+      md: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.4,
+        shadowRadius: 4.65,
+        elevation: 4,
+      },
+      lg: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 5 },
+        shadowOpacity: 0.5,
+        shadowRadius: 6.27,
+        elevation: 6,
+      },
+    },
+  };
