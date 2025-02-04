75e5c4bd90512bd6ed77fb62ad15f69805c27f49
Aidan Lynde <130251073+aidanlynde@users.noreply.github.com>
2025-02-04 17:16:19 -0600

    Create typography.ts
    Init themes

---
+++ src/theme/typography.ts
@@ -0,0 +1,18 @@
+// src/theme/typography.ts
+export const typography = {
+    sizes: {
+      xs: 12,
+      sm: 14,
+      md: 16,
+      lg: 18,
+      xl: 20,
+      xxl: 24,
+      xxxl: 32,
+    },
+    weights: {
+      regular: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
+    },
+  };
