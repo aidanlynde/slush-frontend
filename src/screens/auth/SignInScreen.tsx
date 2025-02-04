f4895509aa8cdc15d37443ec6f23c72f36523048
Aidan Lynde <130251073+aidanlynde@users.noreply.github.com>
2025-02-04 17:44:46 -0600

    Create Screens
    Create placeholder code for app screens for now

---
+++ src/screens/auth/SignInScreen.tsx
@@ -0,0 +1,11 @@
+// src/screens/auth/SignInScreen.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const SignInScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Sign In Screen</Text>
+    </View>
+  );
+};
