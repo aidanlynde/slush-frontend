909ef15eaf8e2756f09005dfef8fbad3d2bb94f9
Aidan Lynde <130251073+aidanlynde@users.noreply.github.com>
2025-02-06 13:43:26 -0600

    Update types.ts
    update type declarations for user signin/signup flow

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,10 +1,6 @@
 // src/navigation/types.ts
 export type RootStackParamList = {
-  Auth: undefined;
-};
-
-export type AuthStackParamList = {
   SignIn: undefined;
   SignUp: undefined;
+  Home: undefined;
 };
-
